73edcf820371b5dd6584cfa57056315dbadde706
Dominik Sigmund <dominik.sigmund@br.de>
2022-11-21 19:57:34 +0100

    #1 Fix description text

--- src/app/changelog/changelog.component.ts
+++ src/app/changelog/changelog.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChangelogComponent implements OnInit {
   changes = [
+    "1.0.39;Anpassung der Webseiten-Beschreibung beim Teilen",
     "1.0.38;Anpassung der Datenschutzhinweise",
     "1.0.37;Link zum Play-Store hinzugefügt",
     "1.0.36;App Store Image gefixt",
