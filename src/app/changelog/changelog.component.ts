0d896f990d05bf0ca44298e243cf5dcfb5f2d900
Dominik Sigmund <dominik.sigmund@br.de>
2022-11-25 15:51:02 +0100

    New Logos

--- src/app/changelog/changelog.component.ts
+++ src/app/changelog/changelog.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChangelogComponent implements OnInit {
   changes = [
+    "1.0.41;Anpassung der Logos",
     "1.0.40;Wechsel des Inhaltlich Verantworlichen auf Oliver Klein",
     "1.0.39;Anpassung der Webseiten-Beschreibung beim Teilen",
     "1.0.38;Anpassung der Datenschutzhinweise",
