7f9c094af79c4c2771bccbee863678890f0cf93a
Dominik Sigmund <dominik.sigmund@br.de>
2022-11-28 15:26:03 +0100

    Fix Wording to Das Bibel Quiz

--- src/app/changelog/changelog.component.ts
+++ src/app/changelog/changelog.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChangelogComponent implements OnInit {
   changes = [
+    "1.0.43;Anpassung Wording",
     "1.0.42;Punktezähler-Problem gelöst",
     "1.0.41;Anpassung der Logos",
     "1.0.40;Wechsel des Inhaltlich Verantworlichen auf Oliver Klein",
